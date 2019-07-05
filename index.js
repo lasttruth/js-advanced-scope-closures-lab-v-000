5260f4141b46cd8516d3735d38f20b9a9685995b
Robin Doirin <robindoirin@gmail.com>
2019-07-05 14:53:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,135 +1,30 @@
-let neighborhoodId = 0;
-let customerId = 0;
-let mealId = 0;
-let deliveryId = 0;
-
-class Neighborhood {
-  constructor(name) {
-    this.name = name;
-    this.id = ++neighborhoodId;
-    store.neighborhoods.push(this);
-  }
-
-  deliveries() {
-    return store.deliveries.filter(
-      function(delivery) {
-        return delivery.neighborhoodId === this.id;
-      }.bind(this)
-    );
-  }
-
-  customers() {
-    return store.customers.filter(
-      function(customer) {
-        return customer.neighborhoodId === this.id;
-      }.bind(this)
-    );
-  }
-
-  meals() {
-    const mealList = this.deliveries().map(
-        function(delivery) {
-          return delivery.meal()
-        }
-      );
-    let uniqueMeals = Array.from(new Set(mealList));
-    return uniqueMeals
-  }
-}
-
-class Customer {
-  constructor(name, neighborhoodId) {
-    this.name = name
-    this.id = ++customerId
-    this.neighborhoodId = neighborhoodId
-    store.customers.push(this)
-  }
-
-  deliveries() {
-    return store.deliveries.filter(
-      function(delivery) {
-        return delivery.customerId === this.id;
-      }.bind(this)
-    );
-  }
-
-  meals() {
-    return this.deliveries().map(
-      function (delivery) {
-        return delivery.meal()
-      }
-    );
-  }
-
-  totalSpent() {
-    return this.meals().reduce(function (agg, meal) {
-      return agg + meal.price;
-      }, 0);
+function produceDrivingRange(blockRange) {
+  return function(arg1, arg2) {
+    let range = parseInt(arg2) - parseInt(arg1);
+    if (range < blockRange ) {
+      let inRange = blockRange - range
+      return `within range by ${inRange}`
+    }
+    else {
+      let outOfRange = range - blockRange
+      return `${outOfRange} blocks out of range`
     }
   }
+}
 
-
-class Meal {
-  constructor(title, price) {
-    this.title = title;
-    this.price = price;
-    this.id = ++mealId;
-    store.meals.push(this);
-  }
-
-  deliveries() {
-    return store.deliveries.filter(
-      function(delivery) {
-        return delivery.mealId === this.id;
-      }.bind(this)
-    );
-  }
-
-  customers() {
-    return this.deliveries().map(
-      function (delivery) {
-        return delivery.customer()
-      }
-    );
-  }
-
-  static byPrice() {
-    return store.meals.sort(function (a, b) {
-      return b.price - a.price;
-    });
+function produceTipCalculator(tipPercent) {
+  return function(bill) {
+    return bill * tipPercent
   }
 }
 
-class Delivery {
-  constructor(mealId, neighborhoodId, customerId) {
-    this.id = ++deliveryId
-    this.mealId = mealId
-    this.neighborhoodId = neighborhoodId
-    this.customerId = customerId
-    store.deliveries.push(this)
-  }
+function createDriver() {
+  let DriverId = 0;
 
-  meal() {
-    return store.meals.find(
-      function(meal) {
-        return meal.id === this.mealId;
-      }.bind(this)
-    );
-  }
-
-  customer() {
-    return store.customers.find(
-      function(customer) {
-        return customer.id === this.customerId;
-      }.bind(this)
-    )
-  }
-
-  neighborhood() {
-    return store.neighborhoods.find(
-      function(neighborhood) {
-        return neighborhood.id === this.neighborhoodId;
-      }.bind(this)
-    );
+  return class {
+    constructor(name) {
+      this.name = name;
+      this.id = ++DriverId;
+    }
   }
 }
